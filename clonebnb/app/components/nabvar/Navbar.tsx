dd9f3f8d8f8a9c2338288e6b0cae43e4feefb408
Kat <kat.valcarcel@live.com>
2024-04-15 14:52:43 -0500

    Navbar UI

--- clonebnb/app/components/nabvar/Navbar.tsx
+++ clonebnb/app/components/nabvar/Navbar.tsx
@@ -1,15 +1,15 @@
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
-
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
-    return (
-        <div className="fixed w-full bg-white z-10 shadow-sm">
-            <div className="py-4 border-b-[1px]">
-                <Container>
-                    <div
-                    className="
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div
+            className="
                     flex
                     flex-row
                     items-center
@@ -17,15 +17,15 @@ const Navbar = () => {
                     gap-3
                     md:gap-0
                     "
-                    >
-<Logo/>
-<Search></Search>
-                    </div>
-                </Container>
-            </div>
-        </div>
-        
-    );
-}
+          >
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
--- clonebnb/app/page.tsx
+++ clonebnb/app/page.tsx
@@ -2,7 +2,8 @@ import Navbar from "./components/nabvar/Navbar";
 
 export default function Home() {
   return (
-    <div className="text-rose-500 text-2x1">
-      <Navbar></Navbar></div>
+    <div className=" text-2x1">
+      <Navbar></Navbar>
+    </div>
   );
 }
