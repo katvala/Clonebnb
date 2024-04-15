0f220aff261151aaf2f6ad304bf975afedc5dfba
Kat <kat.valcarcel@live.com>
2024-04-15 13:57:48 -0500

    NAvbar

---
+++ clonebnb/app/page.tsx
@@ -0,0 +1,8 @@
+import Navbar from "./components/nabvar/Navbar";
+
+export default function Home() {
+  return (
+    <div className="text-rose-500 text-2x1">
+      <Navbar></Navbar></div>
+  );
+}
